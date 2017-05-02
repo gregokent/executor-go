20024742f4e55c9a72df46423bf5dc9c9192205f
Greg Kent <gregokent@gmail.com>
2017-05-01 22:11:59 -0400

    breaking foreground

--- executor.go
+++ executor.go
@@ -7,10 +7,30 @@ type Executor interface {
 }
 
 type InlineExecutor struct {}
+type TaskItem struct {
+    id int
+    task func()
+}
+type ForegroundExecutor struct {
+    uniqueId int
+    tasklist []TaskItem
+}
 
 func (ie InlineExecutor) Submit(task func()) (int, error) {
     task()
     return 1, nil
+}
+
+func (fe ForegroundExecutor) Submit(task func()) (int, error) {
+    fe.uniqueId++
+    fe.tasklist = append(fe.tasklist, TaskItem { fe.uniqueId, task })
+    return fe.uniqueId, nil
+}
+
+func (fe ForegroundExecutor) Execute() {
+    for _, task := range fe.tasklist {
+        task.task()
+    }
 }
 
 func main() {
@@ -19,5 +39,15 @@ func main() {
     ie.Submit(func() {
         fmt.Printf("hello, inline\n")
     })
+
+    fe := ForegroundExecutor{ 0 , make([]TaskItem, 1) } 
+    fe.Submit(func() {
+        fmt.Printf("hello, foreground %v\n", fe.uniqueId)
+    })
+    fe.Submit(func() {
+        fmt.Printf("hello, foreground %v\n", fe.uniqueId)
+    })
+
+    fe.Execute()
 }
 
